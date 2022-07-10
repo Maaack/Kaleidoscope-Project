2dfdadbbb94061fd20fc0b39533425ad05cec459
tomy204 <tomy5924@gmail.com>
2022-07-11 00:28:36 +0200

    got rid of the bug by restarting godot
    add a condition to not send the signal to change tumbler if the button don't have any tumbler
    
    also moved the file to correct location

--- Scenes/UI/CustomComponents/KaleidoscopeButton.gd
+++ Scenes/UI/CustomComponents/KaleidoscopeButton.gd
@@ -5,4 +5,5 @@ export var tumbler_scene : PackedScene
 
 
 func _on_KaleidoscopeButton_pressed():
-	emit_signal("change_tumbler", tumbler_scene)
+	if tumbler_scene:
+		emit_signal("change_tumbler", tumbler_scene)
