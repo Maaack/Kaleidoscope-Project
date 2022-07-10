3f48c16ab5568ff5264890e03a49efe5e009bfc9
amythyst221 <47961786+amythyst221@users.noreply.github.com>
2022-07-10 00:14:54 -0700

    Kaleidoscope shader
    Run ShaderTest.tscn to see

---
+++ Scenes/Experimental/Amy/ShaderTest.gd
@@ -0,0 +1,10 @@
+extends Sprite
+export var blue = 1.0
+
+func _ready():
+	pass#material.set_shader_param("blue", blue)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
