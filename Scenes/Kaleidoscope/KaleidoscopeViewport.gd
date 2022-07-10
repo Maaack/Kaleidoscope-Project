e5214fdd8f96a50d04df997b249df95830b15e04
tomy204 <tomy5924@gmail.com>
2022-07-10 14:55:24 +0200

    use mouse left/right movement for tumbler rotation
    i make use of the left/right movement of the mouse (instead of angle to the center of the screen)
    the speed ammount of rotation applied in the tumbler is clamped to prevent too high speed
    
    support mouse_captured mouse mode

--- Scenes/Kaleidoscope/KaleidoscopeViewport.gd
+++ Scenes/Kaleidoscope/KaleidoscopeViewport.gd
@@ -1,7 +1,22 @@
 extends ViewportContainer
+const MAX_ROTATION_STEP = 5 # maximum rotation that can be applied at each movement
+const MOUSE_SENSITIVITY = 0.001 # sensitivity of the mouse
+const CAPTURED_MOUSE_SENSITIVITY = 0.5# sensitivity of the mouse in captured mode (somewhat the speed is different in each mode)
+var next_rotation = 0 # the next rotation to apply to the tumbler
 
 
-func _process(_delta : float) -> void:
-	var center_point : Vector2 = rect_size / 2.0
-	var mouse_angle : float = center_point.angle_to_point(get_global_mouse_position())
-	$Viewport/TumblerScene.rotation = mouse_angle
+func _physics_process(delta : float) -> void:
+	$Viewport/TumblerScene.rotate(next_rotation * delta)
+	next_rotation = 0
+
+
+func _input(event : InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var x_speed : float
+		
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			x_speed = event.relative.x * CAPTURED_MOUSE_SENSITIVITY
+		else:
+			x_speed = event.speed.x * MOUSE_SENSITIVITY
+		print(x_speed)
+		next_rotation = clamp(x_speed, -MAX_ROTATION_STEP, MAX_ROTATION_STEP)
