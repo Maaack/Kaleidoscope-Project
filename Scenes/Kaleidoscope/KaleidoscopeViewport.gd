c0b2b4346d6fa2c4aee8b94d3b07b69b830f2132
tomy204 <tomy5924@gmail.com>
2022-07-10 15:55:17 +0200

    add controlls for mouse wheel and keyboard (A and D for QWERTY / Q and D for AZERTY layout)
    +remove debug print

--- Scenes/Kaleidoscope/KaleidoscopeViewport.gd
+++ Scenes/Kaleidoscope/KaleidoscopeViewport.gd
@@ -1,7 +1,9 @@
 extends ViewportContainer
+const ROTATION_STEP = 3 # rotation step when using keyboard or scroll wheel to move the kaleidoscope
 const MAX_ROTATION_STEP = 5 # maximum rotation that can be applied at each movement
 const MOUSE_SENSITIVITY = 0.001 # sensitivity of the mouse
 const CAPTURED_MOUSE_SENSITIVITY = 0.5# sensitivity of the mouse in captured mode (somewhat the speed is different in each mode)
+
 var next_rotation = 0 # the next rotation to apply to the tumbler
 
 
@@ -18,5 +20,11 @@ func _input(event : InputEvent) -> void:
 			x_speed = event.relative.x * CAPTURED_MOUSE_SENSITIVITY
 		else:
 			x_speed = event.speed.x * MOUSE_SENSITIVITY
-		print(x_speed)
 		next_rotation = clamp(x_speed, -MAX_ROTATION_STEP, MAX_ROTATION_STEP)
+	
+
+	elif event is InputEventMouseButton or event is InputEventKey:
+		if event.get_action_strength("turn_tumbler_right") > 0: #using get_action_strength instead of is_action_pressed so you can keep the key pressed
+			next_rotation = ROTATION_STEP
+		elif event.get_action_strength("turn_tumbler_left") > 0:
+			next_rotation = -ROTATION_STEP
