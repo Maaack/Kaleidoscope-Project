7b402d737eb7931c7d207da9dc21645ce111daf9
mikezimean <mzimean@gmail.com>
2022-07-12 00:36:09 +0300

    Godot project with Wwise integration settings
    I pray to all kinds of deities and demons it works now

---
+++ wwise/GeneratedSoundBanks/wwise_ids.gd
@@ -0,0 +1,79 @@
+class_name AK
+
+class EVENTS:
+
+	const LEVEL1_LOADED = 1001937282 
+
+	const _dict = { 
+	 "LEVEL1 LOADED": LEVEL1_LOADED
+	} 
+
+class STATES:
+
+	const _dict = {} 
+
+class SWITCHES:
+
+	class MUSIC_CALMINTENSEMAX:
+		const GROUP = 397118060 
+
+		class SWITCH:
+			const CALM = 3753286132 
+			const INTENSE = 4223512837 
+			const MAX = 1048449613 
+
+	const _dict = { 
+		"MUSIC_CALMINTENSEMAX": {
+			"GROUP": 397118060,
+			"SWITCH": {
+				"CALM": 3753286132,
+				"INTENSE": 4223512837,
+				"MAX": 1048449613
+			} 
+		} 
+	} 
+
+class GAME_PARAMETERS:
+
+	const _dict = {} 
+
+class TRIGGERS:
+
+	const _dict = {} 
+
+class BANKS:
+
+	const INIT = 1355168291 
+	const KSCOPEMAIN = 1509027703 
+
+	const _dict = { 
+	 "INIT": INIT,
+	 "KSCOPEMAIN": KSCOPEMAIN
+	} 
+
+class BUSSES:
+
+	const MASTER_AUDIO_BUS = 3803692087 
+
+	const _dict = { 
+	 "MASTER AUDIO BUS": MASTER_AUDIO_BUS
+	} 
+
+class AUX_BUSSES:
+
+	const _dict = {} 
+
+class AUDIO_DEVICES:
+
+	const SYSTEM = 3859886410 
+	const NO_OUTPUT = 2317455096 
+
+	const _dict = { 
+	 "SYSTEM": SYSTEM,
+	 "NO OUTPUT": NO_OUTPUT
+	} 
+
+class EXTERNAL_SOURCES:
+
+	const _dict = {} 
+
