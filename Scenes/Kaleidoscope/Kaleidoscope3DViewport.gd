fda50b857b4437e4527ad1de4869b1415138ddc3
Maaack <marek.belski@gmail.com>
2022-07-11 23:26:33 -0700

    Setup base 3D Viewport.

--- Scenes/Kaleidoscope/Kaleidoscope3DViewport.gd
+++ Scenes/Kaleidoscope/Kaleidoscope3DViewport.gd
@@ -6,5 +6,5 @@ const ROTATION_3D_MOD = 4
 var next_rotation = 0 # the next rotation to apply to the tumbler
 
 func _physics_process(delta : float) -> void:
-	$TumblerScene.rotate(next_rotation * delta)
+	$TumblerScene/CircularTumbler3D.rotate_z(next_rotation * delta * ROTATION_3D_MOD)
 	next_rotation = 0
