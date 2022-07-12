9e85fac6b0e12d58429d44a236775e9ebe93c5e3
amythyst221 <47961786+amythyst221@users.noreply.github.com>
2022-07-11 20:16:10 -0700

    fix tumbler rotation speed
    Added a second step side for auto rotation so these can be controlled separately. Tumbler rotation now works as before.

--- Scenes/Kaleidoscope/KaleidoscopeViewport.gd
+++ Scenes/Kaleidoscope/KaleidoscopeViewport.gd
@@ -1,5 +1,6 @@
 extends ViewportContainer
-const ROTATION_STEP = 0.5# rotation step when using keyboard or scroll wheel to move the kaleidoscope
+const INPUT_ROTATION_STEP = 3.0# rotation step when using keyboard or scroll wheel to move the kaleidoscope
+const AUTO_ROTATION_STEP = 0.5# rotation step when auto rotating
 const MAX_ROTATION_STEP = 5 # maximum rotation that can be applied at each movement
 const MOUSE_SENSITIVITY = 0.001 # sensitivity of the mouse
 const CAPTURED_MOUSE_SENSITIVITY = 0.5# sensitivity of the mouse in captured mode (somewhat the speed is different in each mode)
@@ -11,7 +12,7 @@ var next_rotation = 0 # the next rotation to apply to the tumbler
 
 func _physics_process(delta : float) -> void:
 	if auto_rotate:
-		next_rotation = ROTATION_STEP
+		next_rotation = AUTO_ROTATION_STEP
 	$Viewport/TumblerScene.rotate(next_rotation * delta)
 	next_rotation = 0
 
@@ -32,6 +33,6 @@ func _input(event : InputEvent) -> void:
 
 	elif event is InputEventMouseButton or event is InputEventKey:
 		if event.get_action_strength("turn_tumbler_right") > 0: #using get_action_strength instead of is_action_pressed so you can keep the key pressed
-			next_rotation = ROTATION_STEP
+			next_rotation = INPUT_ROTATION_STEP
 		elif event.get_action_strength("turn_tumbler_left") > 0:
-			next_rotation = -ROTATION_STEP
+			next_rotation = -INPUT_ROTATION_STEP
