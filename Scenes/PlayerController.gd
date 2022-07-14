dc43605488b67431e14740a233210ff2ee53c6f2
tomy204 <tomy5924@gmail.com>
2022-07-14 20:08:08 +0200

    added a raycast to the player controller and ViewCollider
    both use the collision layer 2
    
    the collider use a "collider_type" export var which is an enum to be used as a kind of group to distinguish the color to be affected by it (not sure if absolutely usefull but it's there)
    
    they also provides 2 signals "looked_at" and "stop_looked_at" which are triggered by the LookAtRaycast when it picks it
    
    the LookAtRaycast also provide signal to communicate when it start looking at a new target (including null if looking at nothing in particular)
    
    can be tested in DreamScapeEnvironnment or MainKaleidoscope (need to press k to disable the kaleidoscope in order to see the text appear)

---
+++ Scenes/PlayerController.gd
@@ -0,0 +1,14 @@
+extends Spatial
+
+signal started_looking_at(object)
+
+var looking_at : PhysicsBody
+
+func _physics_process(_delta):
+	if looking_at != $Player/Pivot/Camera/LookingAtRayCast.get_collider():
+		if looking_at:
+			looking_at.emit_signal("stop_looked_at")
+		looking_at = $Player/Pivot/Camera/LookingAtRayCast.get_collider()
+		emit_signal("started_looking_at", looking_at)
+		if looking_at:
+			looking_at.emit_signal("looked_at")
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -52,3 +52,4 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector3.UP, true)
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = jump_force
+
