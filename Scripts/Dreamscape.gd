8f67cc334f970c079778ca7e682cdadd06833993
tomy204 <tomy5924@gmail.com>
2022-07-14 16:25:37 +0200

    Pause menu + PauseMenuController
    Pause menu where you can go to main menu, access the option menu, got to main menu and quit the game
    
    added a script PauseMenuController.gd autoloaded via project settings
    
    to pause the game and display the pause menu from anywhere, set PauseMenuController.paused to true
    
    set it to false to unpause the game and make the menu disappear
    
    i have modified the code in DreamscapeEnvironment to do this instead of quit

--- Scripts/Dreamscape.gd
+++ Scripts/Dreamscape.gd
@@ -1,5 +1,11 @@
 extends Spatial
 
-func _process(delta):
-	if Input.is_action_pressed("ui_cancel"):
-		get_tree().quit()
+var can_pause = true # to prevent from instantly repausing after unpaused from the pause menu by pressing ui_cancel
+
+func _process(_delta):
+	# cannot repause before releasing the key once
+	if Input.is_action_just_pressed("ui_cancel") and can_pause:
+		can_pause = false
+		PauseMenuController.paused = true
+	else:
+		can_pause = true
