4023c6eac965c1a72e4d79b7504d062dc4fe71a4
tomy204 <tomy5924@gmail.com>
2022-07-15 18:48:02 +0200

    created a mushroom with placeholder (yellow cube)
    turns blue when you look at it, you can interract with it by pressing E
    
    this will toggle on the kaleidoscope effect and trigger the sound effect of eating a mushroom

--- Scenes/Kaleidoscope.gd
+++ Scenes/Kaleidoscope.gd
@@ -10,3 +10,7 @@ func _process(delta):
 	if Input.is_action_just_pressed("toggle_show_kaleidoscope",true):
 		var enabled = material.get_shader_param("enabled")
 		material.set_shader_param("enabled", !enabled)
+
+
+func _on_Dreamscape_mushroom_eated():
+	material.set_shader_param("enabled", true)
