2c55e9d800648412295d43df9970bb1b0d21f5d6
Maaack <marek.belski@gmail.com>
2022-07-16 15:54:26 -0700

    Cherry picking changes.

---
+++ Scenes/UI/TextDisplay.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+const min_display_time = 2 # in seconds
+const display_time_ratio = 0.1 # how many time should the text be displayed by number of characters
+
+func display_text(text : String) -> void:
+	$Timer.stop()
+	$Label.text = text
+	$Timer.start( max(text.length() * display_time_ratio, min_display_time) )
+	$AnimationPlayer.play("FadeIn")
+
+
+func _on_Timer_timeout():
+	$AnimationPlayer.play_backwards("FadeIn")
+
+func started_looking_at(object : ViewCollider) -> void:
+	if object == null:
+		return
+	display_text("Looking at %s" % ViewCollider.Type.keys()[object.collider_type])
