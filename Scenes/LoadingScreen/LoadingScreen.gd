190f21af718698f3dffe86d68de245d86cfd6a4c
Maaack <marek.belski@gmail.com>
2022-08-02 21:55:46 -0700

    Adds momentary delay of loading next scene to show 100%.

--- Scenes/LoadingScreen/LoadingScreen.gd
+++ Scenes/LoadingScreen/LoadingScreen.gd
@@ -3,7 +3,6 @@ extends Control
 
 onready var loader = ResourceLoader.load_interactive(SceneLoader.scene_to_load)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var err = loader.poll()
@@ -11,6 +10,8 @@ func _process(delta):
 		$VBoxContainer/ProgressBar.value = loader.get_stage() * 100 / loader.get_stage_count()
 	elif err == ERR_FILE_EOF:
 		$VBoxContainer/ProgressBar.value = 100
+		set_process(false)
+		yield(get_tree().create_timer(0.1), "timeout")
 		get_tree().change_scene_to(loader.get_resource())
 	else:
 		$ErrorMsg.dialog_text = "Error: %d" % err
